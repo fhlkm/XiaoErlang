0ea4586ae93b0cb284a09682e8c1970b28a74d59
Hanlu Feng <hanlu.feng@sisa.samsung.com>
2017-02-14 18:44:04 -0500

    Add signup

--- XiaoErLang/app/src/main/java/com/example/username/xiaoerlang/LoginActivity.java
+++ XiaoErLang/app/src/main/java/com/example/username/xiaoerlang/LoginActivity.java
@@ -2,12 +2,25 @@ package com.example.username.xiaoerlang;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText mUserName;
+    private EditText mPassword;
+    private Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+        init();
     }
+    private void init(){
+        mUserName = (EditText)findViewById(R.id.input_email);
+        mPassword = (EditText)findViewById(R.id.input_password);
+        login = (Button)findViewById(R.id.btn_login);
+    }
+
 }
